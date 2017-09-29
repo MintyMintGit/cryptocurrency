5a4684f7f7fe9b9e351397d752d671ea1c47721d
Anton <antonm1.manzhos@gmail.com>
2017-09-29 18:30:21 +0300

    added dataTable

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    $('#myTable').DataTable();
+    $('#marketCapitalizations').DataTable({
+        "ajax": "https://api.coinmarketcap.com/v1/ticker/?limit=1000",
+        "columns": [
+            {"data": "rank"},
+            {"data": "name"},
+            {"data": "market_cap_usd"},
+            {"data": "price_usd"},
+            {"data": "total_supply"},
+            {"data": "24h_volume_usd"},
+            {"data": "percent_change_24h"},
+            {"data": ""}
+        ]
+    });
 });
