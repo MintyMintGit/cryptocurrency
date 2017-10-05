02f1e5f9871f6a194a03f40282b31b75c0c8d5bb
Anton <antonm1.manzhos@gmail.com>
2017-10-05 17:22:53 +0300

    #11 1)optimization

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,5 +1,5 @@
 var currencyExchangeRates = [];
-
+var searchListItems = "";
 function getExchangeRates() {
     $.ajax({
         url: $("#ExchangeRatesLink").val(),
@@ -19,28 +19,17 @@ $(document).ready(function () {
     $("#calculatorTab").addClass("active");
     getExchangeRates();
 
-    $("#from").on('keyup', function (event) {
-        $("#autoFrom li").remove();
+    $("#to, #from").on('keyup', function (event) {
+        var currentItem = $(event.currentTarget);
+        searchListItems = currentItem.siblings('ul');
+        searchListItems.html('');
         var key = event.currentTarget.value.toUpperCase();
         var hardcoded = $("#hardcoded li").clone();
-        $("#autoFrom").append(hardcoded);
+        searchListItems.append(hardcoded);
         $.each(currencyExchangeRates, function (indx, element) {
             if(currencyExchangeRates[indx].substring(0, key.length) == key) {
-                $("#autoFrom").append("<li>" + currencyExchangeRates[indx] + "</li>")
+                searchListItems.append("<li>" + currencyExchangeRates[indx] + "</li>")
             }
         });
     });
-
-    $("#to").on('keyup', function (event) {
-        $("#autoTo li").remove();
-        var key = event.currentTarget.value.toUpperCase();
-        var hardcoded = $("#hardcoded li").clone();
-        $("#autoTo").append(hardcoded);
-        $.each(currencyExchangeRates, function (indx, element) {
-            if(currencyExchangeRates[indx].substring(0, key.length) == key) {
-                $("#autoTo").append("<li>" + currencyExchangeRates[indx] + "</li>")
-            }
-        });
-    });
-
 });
