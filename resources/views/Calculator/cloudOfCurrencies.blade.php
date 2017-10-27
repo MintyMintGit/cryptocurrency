89bd6e195d50fdba808fb5cc15a338758a4cb2ea
antonmManzhos <antonm1.manzhos@gmail.com>
2017-10-27 12:15:36 +0300

    added calculator

--- resources/views/Calculator/cloudOfCurrencies.blade.php
+++ resources/views/Calculator/cloudOfCurrencies.blade.php
@@ -2,19 +2,17 @@
     <div class="container">
 
 @if ($CloudsOfCurrencies->count() > 0)
-        <?php $counter = 0; ?>
-            @foreach($CloudsOfCurrencies as $Currenci)
-                @if ($Currenci['type'] == 'crypto' )
-                    <a data-content="{{ $counter }}" class="{{ isset($Currenci['topPopular']) ? "topPopular":"" }} {{ isset($Currenci['lessPopular']) ? "lessPopular":"" }}" href="/crypto/{{ $Currenci['profile_long'] != "" ? strtolower(str_replace(' ', '-',$Currenci['profile_long'])) : strtolower(str_replace(' ', '-',$Currenci['id'])) }}">
-                        {{ $Currenci['profile_long'] != "" ? $Currenci['profile_long'] : $Currenci['id'] }}
-                    </a>
-                @else
-                    <a data-content="{{ $counter }}" class="{{ isset($Currenci['topPopular']) ? "topPopular":"" }} {{ isset($Currenci['lessPopular']) ? "lessPopular":"" }}" href="/world/{{ $Currenci['profile_long'] != "" ? strtolower(str_replace(' ', '-',$Currenci['profile_long'])) : strtolower(str_replace(' ', '-',$Currenci['id'])) }}-exchange-rates">
-                        {{ $Currenci['profile_long'] != "" ? $Currenci['profile_long'] : $Currenci['id'] }}
-                    </a>
-                @endif
-            <?php $counter++ ?>
-            @endforeach
+        @foreach($CloudsOfCurrencies as $Currenci)
+            @if ($Currenci['type'] == 'crypto' )
+                <a data-content="{{ $counter }}" class="{{ isset($Currenci['topPopular']) ? "topPopular":"" }} {{ isset($Currenci['lessPopular']) ? "lessPopular":"" }}" href="/crypto/{{ $Currenci['profile_long'] != "" ? strtolower(str_replace(' ', '-',$Currenci['profile_long'])) : strtolower(str_replace(' ', '-',$Currenci['id'])) }}">
+                    {{ $Currenci['profile_long'] != "" ? $Currenci['profile_long'] : $Currenci['id'] }}
+                </a>
+            @else
+                <a data-content="{{ $counter }}" class="{{ isset($Currenci['topPopular']) ? "topPopular":"" }} {{ isset($Currenci['lessPopular']) ? "lessPopular":"" }}" href="/world/{{ $Currenci['profile_long'] != "" ? strtolower(str_replace(' ', '-',$Currenci['profile_long'])) : strtolower(str_replace(' ', '-',$Currenci['id'])) }}-exchange-rates">
+                    {{ $Currenci['profile_long'] != "" ? $Currenci['profile_long'] : $Currenci['id'] }}
+                </a>
+            @endif
+        @endforeach
 
 @endif
 
