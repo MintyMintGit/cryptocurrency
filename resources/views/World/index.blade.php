10fd62c9287753e48d872c8f339bdbc6fbafc7f3
antonmManzhos <antonm1.manzhos@gmail.com>
2017-10-27 15:22:26 +0300

    added world index page

--- resources/views/World/index.blade.php
+++ resources/views/World/index.blade.php
@@ -2,49 +2,44 @@
 
 @section('content')
 
-    <table id="money" class="display" cellspacing="0" width="100%">
-        <thead>
-        <tr>
-            <th>Name</th>
-            <th>ISO</th>
-            <th>Symbol</th>
-            <th>Central Bank Name</th>
-            <th>Central Bank Website</th>
-            <th>Unit</th>
-            <th>Cent</th>
-            <th>Coin</th>
-            <th>Bank Notes</th>
-        </tr>
-        </thead>
-        <tfoot>
-        <tr>
-            <th>Name</th>
-            <th>ISO</th>
-            <th>Symbol</th>
-            <th>Central Bank Name</th>
-            <th>Central Bank Website</th>
-            <th>Unit</th>
-            <th>Cent</th>
-            <th>Coin</th>
-            <th>Bank Notes</th>
-        </tr>
-        </tfoot>
-        <tbody>
+    @include('layouts.partials._navigation')
+<div class="world_currencies">
+    <div class="container-fluid page-content">
+        <h1>WORLD CURRENCIES</h1>
+        <section class="world-currencies-table">
+            <table id="money">
+                <thead>
+                <tr>
+                    <th class="name" scope="col">Name</th>
+                    <th class="iso" scope="col">ISO</th>
+                    <th class="symbol" scope="col">Symbol</th>
+                    <th class="bank-name" scope="col">Central Bank Name</th>
+                    <th class="bank-site" scope="col">Central Bank Website</th>
+                    <th class="unit" scope="col">Unit</th>
+                    <th class="cent" scope="col">Cent</th>
+                    <th class="coin" scope="col">Coin</th>
+                    <th class="banknotes" scope="col">Bank Notes</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($money as $value)
+                    <tr>
+                        <td class="name"><a href="{{ "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" . "/" . strtolower(str_replace(' ', '-', $value['profile_long'])) . '-exchange-rates' }}">{{ $value['profile_long'] }}</a></td>
+                        <td class="iso">{{ $value['profile_short'] }}</td>
+                        <td class="symbol">{{ $value['profile_symbol'] }}</td>
+                        <td class="bank-name">{{ $value['profile_central_bank_name'] }}</td>
+                        <td class="bank-site">{{ $value['profile_central_bank_website'] }}</td>
+                        <td class="unit">{{ $value['profile_unit'] }}</td>
+                        <td class="cent">{{ $value['profile_cent'] }}</td>
+                        <td class="coin">{{ $value['profile_coins'] }}</td>
+                        <td class="banknotes">{{ $value['profile_banknotes'] }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </section>
+    </div>
 
-        @foreach ($money as $value)
-            <tr>
-                <td><a href="{{ "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" . "/" . strtolower(str_replace(' ', '-', $value['profile_long'])) . '-exchange-rates' }}">{{ $value['profile_long'] }}</a></td>
-                <td>{{ $value['profile_short'] }}</td>
-                <td>{{ $value['profile_symbol'] }}</td>
-                <td>{{ $value['profile_central_bank_name'] }}</td>
-                <td>{{ $value['profile_central_bank_website'] }}</td>
-                <td>{{ $value['profile_unit'] }}</td>
-                <td>{{ $value['profile_cent'] }}</td>
-                <td>{{ $value['profile_coins'] }}</td>
-                <td>{{ $value['profile_banknotes'] }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+</div>
 
 @endsection
