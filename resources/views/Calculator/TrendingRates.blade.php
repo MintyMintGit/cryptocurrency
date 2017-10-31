5ec6fa08a6c94d414a76cc7902e3bc9d4942d436
Anton <antonm1.manzhos@gmail.com>
2017-10-31 14:15:37 +0200

    added lonks to block

--- resources/views/Calculator/TrendingRates.blade.php
+++ resources/views/Calculator/TrendingRates.blade.php
@@ -6,55 +6,69 @@
     <div class="row seven-cols">
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4>
-                    <span class="from"></span>
-                    /
-                    <span class="to">USD</span>
-                </h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4>
+                        <span class="from"></span>
+                        /
+                        <span class="to">USD</span>
+                    </h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4><span class="from"></span>/<span class="to">EUR</span></h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4><span class="from"></span>/<span class="to">EUR</span></h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4><span class="from"></span>/<span class="to">CAD</span></h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4><span class="from"></span>/<span class="to">CAD</span></h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4><span class="from"></span>/<span class="to">CHF</span></h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4><span class="from"></span>/<span class="to">CHF</span></h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4><span class="from"></span>/<span class="to">CNY</span></h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4><span class="from"></span>/<span class="to">CNY</span></h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4><span class="from"></span>/<span class="to">JPY</span></h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4><span class="from"></span>/<span class="to">JPY</span></h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
         <div class="col-lg-1 col-md-1 col-sm-4 col-xs-6">
             <div class="item greyBlock">
-                <h4><span class="from"></span>/<span class="to">EUR</span></h4>
-                <p class="someValue">1,45431</p>
-                <span class="trendingRates">-1.1777971%</span>
+                <a class="linkGreyBlock">
+                    <h4><span class="from"></span>/<span class="to">EUR</span></h4>
+                    <p class="someValue">1,45431</p>
+                    <span class="trendingRates">-1.1777971%</span>
+                </a>
             </div>
         </div>
     </div>
