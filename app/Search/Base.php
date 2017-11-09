79b1dbc90839db6bcf80ece0742780a7f14e6932
Anton <antonm1.manzhos@gmail.com>
2017-11-09 12:29:04 +0200

    updated search - low search ; updated code

--- app/Search/Base.php
+++ app/Search/Base.php
@@ -91,17 +91,7 @@ class Base
         return $exchange_pairs;
     }
     public static function mergeFromDBandGenerated($db_array, $gen_array) {
-//        $db_array = array();
-//        $item['id'] = 'USDEUR';
-//        $item['rate'] = 1;
-//        $item['type'] = 'exchange_pair';
-//        $item['profile_long'] = '';
-//        $item['exchange1'] = 'USD';
-//        $item['exchange2'] = 'EUR';
-//        $db_array[] = $item;
-//
-//
-//        $gen_array = self::generateExchangePairUSD($fiats = Search::where('type', 'fiat')->orderBy('rate', 'DESC')->get()->toArray());
+
         if(count($db_array) && count($gen_array)) {
             foreach ($gen_array as $i => $gen) {
                 $flag = true;
@@ -127,15 +117,15 @@ class Base
     {
         $fiat = Search::where('type', 'fiat')
             ->where('id', 'like', '%' . $str . '%')
-            ->orderBy('rate', 'DESC')->get()->toArray();
+            ->orderBy('rate', 'DESC')->take(2)->get()->toArray();
         return $fiat;
     }
     public static function getAllFiatValues() {
-        return Search::where('type', 'fiat')->orderBy('rate', 'DESC')->get()->toArray();
+        return Search::where('type', 'fiat')->orderBy('rate', 'DESC')->take(2)->get()->toArray();
     }
 
     public static function getAllCryptoValues() {
-        return Search::where('type', 'crypto')->orderBy('rate', 'DESC')->get()->toArray();
+        return Search::where('type', 'crypto')->orderBy('rate', 'DESC')->take(2)->get()->toArray();
     }
 
     public static function mergeSearchWithAll($search_array, $search_all) {
@@ -165,20 +155,15 @@ class Base
 
     public static function searchInPair($str)
     {
-//      затем делаем пары из фиатных - фиатная валюта которая совпала по поиску + все фиаты
         $allFiat = self::getAllFiatValues();
         $searchFiat = self::searchInFiatValues($str);
         $fiat = self::mergeSearchWithAll($searchFiat, $allFiat);
 
-        //теперь пары из крипто
         $allCrypto = self::getAllCryptoValues();
         $searchCrypto = self::searchInCryptoValues($str);
         $crypto = self::mergeSearchWithAll($searchCrypto, $allCrypto);
 
-        //теперь надо оставить только уникальные пары
-        //сначала отдаем пары по рейтингу, ищем только по exchange1
         $pairsExchange1 = Search::where('type', 'exchange_pair')->where('exchange1','like', '%' . $str. '%')->orderBy('rate', 'DESC')->get()->toArray();
-        //$pairsExchange2 = Search::where('type', 'exchange_pair')->where('exchange2','like', '%' . $str. '%')->orderBy('rate', 'DESC')->get()->toArray(); //это в случе если необходимо искать  по второй паре
         $result = self::mergeFromDBandGenerated($pairsExchange1, array_merge($fiat, $crypto));
         $result = array_merge($result, $searchFiat);
         $result = array_merge($result, $searchCrypto);
@@ -187,51 +172,8 @@ class Base
 
     public static function searchInListSearch($str)
     {
-        /*
-         * сначала отдаем пары по рейтингу, ищем только по exchange1
-         * затем делаем пары из фиатных - фиатная валюта которая совпала по поиску + все фиаты
-         * затем крипто пары - крипто которые совпали + все крипто
-         * далее все фиатные совпали
-         * далее все крипто которые совпали
-         * */
-
         if (count($str) > 0) {
             return self::searchInPair($str);
-
-//            $exchange_pairs = [];
-//            $str = strtoupper($str);
-//            $fiat = Search::where('type', 'fiat')->orderBy('rate', 'DESC')->get()->toArray();
-//
-//            foreach ($fiat as $item) {
-//                $item['id'] = $str . $item['id'];
-//                $item['type'] = "exchange_pair";
-//                $item['exchange1'] = $str;
-//                $item['exchange2'] = $item['id'];
-//                $exchange_pairs[] = $item;
-//            }
-//
-//            $crypto = Search::where('type', 'crypto')
-//                ->orderBy('rate', 'DESC')->get()->toArray();
-//
-//            foreach ($crypto as $item) {
-//                $item['id'] = $str . $item['id'];
-//                $item['type'] = "exchange_pair";
-//                $exchange_pairs[] = $item;
-//            }
-//                $cryptoes = Search::where('type', 'crypto')
-//                    ->where('id', 'like', '%' . $str . '%')
-//                    ->orderBy('rate', 'DESC')->get()->toArray();
-//
-//                $exchange_pairs = array_merge($exchange_pairs, $cryptoes);
-//
-//            $fiats = Search::where('type', 'fiat')
-//                ->where('id', 'like', '%' . $str . '%')
-//                ->orderBy('rate', 'DESC')->get()->toArray();
-//
-//
-//            $exchange_pairs = array_merge($exchange_pairs, $fiats);
-//            }
-//            return $exchange_pairs;
         }
     }
 }
