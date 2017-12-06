1f0e3117591d018335f393a9b6e7ab5407698126
antonmManzhos <antonm1.manzhos@gmail.com>
2017-12-06 17:25:08 +0200

    some little fixes

--- resources/views/Calculator/conversionTable.blade.php
+++ resources/views/Calculator/conversionTable.blade.php
@@ -4,13 +4,18 @@
         <tr>
             <td class="text-uppercase">{{ $currencyFrom->shortName }}</td>
             <td class="text-uppercase">{{ $currencyTo->shortName }}</td>
+            <td class="text-uppercase">{{ $currencyFrom->shortName }}</td>
+            <td class="text-uppercase">{{ $currencyTo->shortName }}</td>
         </tr>
         </thead>
         @isset($conversionTable)
             @foreach ($conversionTable as $item)
                 <tr>
-                    <td class="text-uppercase">{{ $item[2] }} {{ $currencyFrom->shortName }}</td>
-                    <td class="text-uppercase">{{ $item[0] }} {{ $currencyTo->shortName }}</td>
+                    <td class="text-uppercase">{{ $item[0] }} {{ $currencyFrom->shortName }} =</td>
+                    <td class="text-uppercase">{{ $item[1] }} {{ $currencyTo->shortName }}</td>
+
+                    <td class="text-uppercase">{{ $item[2] }} {{ $currencyFrom->shortName }} =</td>
+                    <td class="text-uppercase">{{ $item[0] }} {{ $currencyTo->shortName }} </td>
                 </tr>
             @endforeach
         @endisset
